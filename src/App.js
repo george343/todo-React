41006efdf4457f37795bff9e118413bd9b036bc9
george343 <kavalieratos.giwrgos@gmail.com>
2020-10-17 11:45:52 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Column from './Column'
+import NewTaskButton from './NewTaskButton';
+import { faCheck, faCogs, faSadCry, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todo: ["sd"],
+    doing: [],
+    done: []
+  }
+
+  addToDo = (task) => {
+    this.setState({todo: this.state.todo.concat(task)})
+  }
+
+  handleIconsClick = (icon, returnedTask) =>  {
+    switch (icon.iconName) {
+      case "sad-cry":
+        this.setState({
+          todo: this.state.todo.filter(task => task !== returnedTask)
+        })
+        this.setState({doing: this.state.doing.concat(returnedTask)})
+        break;
+      case "check":
+        this.setState({
+          doing: this.state.doing.filter(task => task !== returnedTask)
+        })
+        this.setState({done: this.state.done.concat(returnedTask)})
+        break;
+      case "cogs":
+        this.setState({
+          doing: this.state.doing.filter(task => task !== returnedTask)
+        })
+        this.setState({todo: this.state.todo.concat(returnedTask)})
+        break;
+      case "trash-alt":
+        this.setState({
+          doing: this.state.doing.filter(task => task !== returnedTask),
+          todo: this.state.todo.filter(task => task !== returnedTask)
+        })
+        break;
+      default:
+        break
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header"><h1>ToDo List</h1></header>
+        <Column 
+          title='To Do' 
+          ls={this.state.todo} 
+          icon1={faSadCry}
+          icon3={faTrashAlt} 
+          fun={this.handleIconsClick} />
+        <Column 
+          title='Doing' 
+          ls={this.state.doing} 
+          icon1={faCheck} 
+          icon2={faCogs}
+          icon3={faTrashAlt} 
+          fun={this.handleIconsClick} />
+        <Column 
+          title='Done' 
+          ls={this.state.done} />
+        <NewTaskButton 
+          parentCallBack={this.addToDo} />
+        </div>
+    );
+  }
 }
 
 export default App;
